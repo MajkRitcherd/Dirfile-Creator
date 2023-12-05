2f1384b932f2d4d7f62a754808b18874fb9b23b6
MajkRitcherd <misutak87@gmail.com>
2023-12-05 10:55:51 +0100

    Base layout, radio button functionality

---
+++ Dirfile-Creator_Graphical/Models/MainWindowModel.cs
@@ -0,0 +1,34 @@
+﻿// ||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|| \\
+// ||                                                    || \\
+// ||    <Author>       Majk Ritcherd       </Author>    || \\
+// ||                                                    || \\
+// ||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|| \\
+//                              Last change: 05/12/2023     \\
+
+using Dirfile_lib.API.Extraction.Modes;
+
+namespace Dirfile_Creator_Graphical.Models
+{
+    /// <summary>
+    /// Model for MainWindow.
+    /// </summary>
+    internal class MainWindowModel
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindowModel"/> class.
+        /// </summary>
+        public MainWindowModel()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the path mode to be used in context.
+        /// </summary>
+        public PathMode PathMode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the slash mode to be used in context.
+        /// </summary>
+        public SlashMode SlashMode { get; set; }
+    }
+}
--- Dirfile-Creator_Graphical/Views/MainWindow.xaml.cs
+++ Dirfile-Creator_Graphical/Views/MainWindow.xaml.cs
@@ -3,9 +3,11 @@
 // ||    <Author>       Majk Ritcherd       </Author>    || \\
 // ||                                                    || \\
 // ||~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|| \\
-//                              Last change: 09/08/2023     \\
+//                              Last change: 05/12/2023     \\
 
 using System.Windows;
+using Dirfile_Creator_Graphical.Models;
+using Dirfile_lib.API.Extraction.Modes;
 
 namespace Dirfile_Creator_Graphical.Views
 {
@@ -14,9 +16,50 @@ namespace Dirfile_Creator_Graphical.Views
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
         public MainWindow()
         {
+            this.Model = new MainWindowModel();
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets or sets the model of Main window.
+        /// </summary>
+        private MainWindowModel Model { get; set; }
+
+        /// <summary>
+        /// Changes the path mode to be used in context.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">Event arguments.</param>
+        private void ChangePathMode(object sender, RoutedEventArgs e)
+        {
+            if (RelativeRadioButton.IsChecked.HasValue)
+            {
+                if (RelativeRadioButton.IsChecked.Value)
+                    this.Model.PathMode = PathMode.Relative;
+                else
+                    this.Model.PathMode = PathMode.Absolute;
+            }
+        }
+
+        /// <summary>
+        /// Changes the slash mode to be used in context.
+        /// </summary>
+        /// <param name="sender">Sender.</param>
+        /// <param name="e">Event arguments.</param>
+        private void ChangeSlashMode(object sender, RoutedEventArgs e)
+        {
+            if (BackwardRadioButton.IsChecked.HasValue)
+            {
+                if (BackwardRadioButton.IsChecked.Value)
+                    this.Model.SlashMode = SlashMode.Backward;
+                else
+                    this.Model.SlashMode = SlashMode.Forward;
+            }
         }
     }
 }
